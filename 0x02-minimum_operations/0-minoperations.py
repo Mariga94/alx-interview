cc659582c3b1dc41b3e48e725dfd36a57cc6d3b3
joseph <jsphmariga@gmail.com>
2023-01-12 09:20:26 +0000

    Improve

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,19 +7,18 @@
 def minOperations(n):
     """Calculates the fewest number of operations needed
     to result in exactly n H characters in the file
-    
+
     Args:
       n (int): number of H
 
     Return:
         minimum number of operations
     """
-    minimumOp = 0
+    minO = 0
     m = 2
-    while m > 1:
-      while n % m:
-        m += 1
-      minimumOp += m
-      n = int(n / m)
-    return minimumOp 
-
+    while isinstance(n, int) and n > 1:
+        while n % m:
+            m += 1
+        minO += m
+        n = int(n / m)
+    return minO
