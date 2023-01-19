36e3a37f605bda413ae323892580f38ece1f101e
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-01-20 02:56:17 +0300

    Fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,40 +2,32 @@
 """Log Parsing Module"""
 import sys
 
-possible_status_codes = {200: 0, 301: 0, 400: 0,
-                         401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {'200': 0, '301': 0, '400': 0,
+                         '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-
-
-def parse_line(line):
-    """Parses line from file"""
-
-    try:
-        char = line.rstrip().split(' ')
-        total_size += char[-1]
-
-        if char[-2] in possible_status_codes:
-            possible_status_codes[char[-2]] += 1
-    except Exception:
-        pass
 
 
 def print_stats():
     """Prints all current stats"""
     print("File size: {}".format(total_size))
-    for k in sorted(possible_status_codes.keys()):
-        if possible_status_codes[k]:
-            print("{}: {}".format(k,  possible_status_codes[k]))
-
-n = 1
+    for k in sorted(status_codes.keys()):
+        if status_codes[k]:
+            print("{}: {}".format(k,  status_codes[k]))
 
 try:
-    for line in sys.stdin:
-        parse_line(line)
-        if n % 10 == 0:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
+        try:
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+            total_size.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
             print_stats()
-            n += 1
+    print_stats()
 except KeyboardInterrupt:
     print_stats()
     raise
-print_stats()
