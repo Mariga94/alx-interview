12b8b43a3370e2bf95a068687a1db37054e7231a
joseph <jsphmariga@gmail.com>
2023-02-10 00:37:50 +0000

    Improve

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,20 +2,27 @@
 const request = require('request');
 const movieId = process.argv[2];
 const starWarsAPI = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-if (movieId) {
-  request(`${starWarsAPI}`, function (error, res, body) {
-    if (error) {
-      console.log('Error');
-    } else {
-      const bodyObj = JSON.parse(body);
-      const charactersURL = bodyObj.characters;
-      charactersURL.forEach(characterURL => {
-        request(`${characterURL}`, function (body, res) {
-          console.log(JSON.parse(res.body).name);
-        });
-      });
-    }
-  });
-} else {
-  console.log('Enter the correct index value');
-}
+request(starWarsAPI, function (error, res, body) {
+  if (error) {
+    console.log('Error', error);
+  }
+  const charactersURL = JSON.parse(body).characters;
+  const size = charactersURL.length;
+  const array = Array(size).fill();
+  let count = 0;
+  for (let i = 0; i < size; i++) {
+    request(charactersURL[i], function (error, res, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        array[i] = JSON.parse(body).name;
+        count++;
+      }
+      if (count === size) {
+        for (let j = 0; j < size; j++) {
+          console.log(array[j]);
+        }
+      }
+    });
+  }
+});
