3c447fef9c9f5b1cd2a7ef838ab015a20d037652
joseph <jsphmariga@gmail.com>
2023-02-24 01:51:14 +0000

    Fix pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,7 @@
 
 
 def makeChange(coins, total):
-    """Calculates the fewest number of coins needed to 
+    """Calculates the fewest number of coins needed to
     return a total
     Arguments:
         coins (int): collection of selected coins
@@ -13,11 +13,10 @@ def makeChange(coins, total):
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    for a in range(1, total +1):
+    for a in range(1, total + 1):
         for coin in coins:
             if a - coin >= 0:
                 dp[a] = min(dp[a], 1 + dp[a - coin])
     if dp[total] != total + 1:
         return dp[total]
     return -1
-
